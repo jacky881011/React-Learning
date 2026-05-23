3f1f2a6abb4f16488efbbc87a12b31bde388a3b2
mingyun <jacky.hsu.tw1@gmail.com>
2026-05-23 12:42:47 +0800

    inline style of css file generate

--- my-react-project/src/Button/Button.jsx
+++ my-react-project/src/Button/Button.jsx
@@ -3,8 +3,18 @@ import styles from "./Button.module.css";
 
 function Button() {
 
+    const param_styles = {
+        backgroundColor: "hsl(149, 81.70%, 44.90%)",
+        color: "Black",
+        padding: "10px 20px",
+        borderRadius: "5px",
+        border: "none",
+        cursor: "pointer",    
+    };
+
     return(
-        <button className={styles.button}>Click Me</button>
+        //<button className={styles.button}>Click Me</button>
+        <button style={param_styles}>Click Me</button>
     )
 
 }
