5290f8c8e95dab1df2687e145799a0b472f56d62
mingyun <jacky.hsu.tw1@gmail.com>
2026-05-23 13:18:05 +0800

    add node module in project - propTypes (npm install prop-types) in terminal

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -3,7 +3,10 @@ import Student from "./Student";
 function App() {
   return(
     <>
-      <Student name="Hello my friend" age = {40} isStudent= {true}/>
+      <Student name="Jacky" age = {35} isStudent= {false}/>
+      <Student name="Bob" age = {20} isStudent= {true}/>
+      <Student name="Alisa" age = {12} isStudent= {true}/>
+      <Student name="Alisa" age = {58} isStudent= {false}/>
     </>
   )
 }
--- my-react-project/src/Student.jsx
+++ my-react-project/src/Student.jsx
@@ -1,15 +1,21 @@
-
+import PropTypes from 'prop-types'
 // use props to pass data and be js project
 // if is boolean use operator 
 function Student(props){
     return(
-        <div>
+        <div className = "student">
             <p>Name: {props.name}</p>
             <p>Age: {props.age}</p>
-            <p>Is student: {props.isStudent? "Yes": "No"}</p>
+            <p>Is student: {props.isStudent ? "Yes": "No"}</p>
         </div>
     );
 
+}
+
+Student.propTypes = {
+    name: PropTypes.string,
+    age: PropTypes.number,
+    isStudent: PropTypes.bool
 }
 
 export default Student;
