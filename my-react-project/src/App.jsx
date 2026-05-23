5e7d0d096011ad9b5c07ce1835f9391ffeaaefdf
mingyun <jacky.hsu.tw1@gmail.com>
2026-05-23 15:07:09 +0800

    MileStone6: Learn how to use List (Sort, Filter) and use Dictionary Key value skills

--- my-react-project/src/App.jsx
+++ my-react-project/src/App.jsx
@@ -1,9 +1,24 @@
 // import Button from "./Button/Button";
 import List from "./List";
 function App() {
+
+  const insertList = [{id:1, name:"Apple", color:"Red", calories: 95}, 
+    {id:2, name:"Orange", color:"Orange", calories: 62}, 
+    {id:3, name:"Banana", color:"Yellow", calories: 100}, 
+    {id:4, name:"Cherry", color:"Red", calories: 204}, 
+    {id:5, name:"Grapes", color:"Purple", calories: 3}];
+
+
+  const insertList2 = [{id:8, name:"Apple", color:"Red", calories: 95}, 
+      {id:9, name:"Orange", color:"Orange", calories: 62}, 
+      {id:10, name:"Banana", color:"Yellow", calories: 100}, 
+      {id:11, name:"Cherry", color:"Red", calories: 204}, 
+      {id:12, name:"Grapes", color:"Purple", calories: 3}];
+
   return(
     <>
-     <List />
+     {insertList.length > 0 && <List items = {insertList} category = "fruit"/>}
+     {insertList2.length > 0 && <List items = {insertList2} category = "fruit"/>}
     </>
   )
 }
--- my-react-project/src/List.jsx
+++ my-react-project/src/List.jsx
@@ -1,28 +1,55 @@
-function List()
-{
-    const fruits = ["Apple", "Orange", "Banana", "Cherry", "Grapes"]; 
+import PropTypes from "prop-types";
 
-    const fruits_dict = [{id:1, name:"Apple", color:"Red", calories: 95}, 
-                        {id:2, name:"Orange", color:"Orange", calories: 62}, 
-                        {id:3, name:"Banana", color:"Yellow", calories: 50}, 
-                        {id:4, name:"Cherry", color:"Red", calories: 4}, 
-                        {id:5, name:"Grapes", color:"Purple", calories: 3}];
+function List(props)
+{
+    // const fruits = ["Apple", "Orange", "Banana", "Cherry", "Grapes"]; 
+
+    // const fruits_dict = [{id:1, name:"Apple", color:"Red", calories: 95}, 
+    //                     {id:2, name:"Orange", color:"Orange", calories: 62}, 
+    //                     {id:3, name:"Banana", color:"Yellow", calories: 50}, 
+    //                     {id:4, name:"Cherry", color:"Red", calories: 4}, 
+    //                     {id:5, name:"Grapes", color:"Purple", calories: 3}];
     
+    const insertList = props.items
     // fruits.sort()
 
     // defenition of sort() method: The sort() method sorts the elements of an array in place and returns the sorted array. The default sort order is ascending, built upon converting the elements into strings, then comparing their sequences of UTF-16 code units values.
-    fruits_dict.sort((a,b)=>{
-        if(a.calories < b.calories) return -1;
-        else if(a.calories > b.calories) return 1;
-        else return 0;})
+    // fruits_dict.sort((a,b)=>{
+    //     if(a.calories < b.calories) return -1;
+    //     else if(a.calories > b.calories) return 1;
+    //     else return 0;})
 
-    fruits_dict.sort((a,b)=> a.name.localeCompare(b.name));
-    fruits_dict.sort((a,b)=> b.name.localeCompare(a.name));
+    // fruits_dict.sort((a,b)=> a.name.localeCompare(b.name));
+    // fruits_dict.sort((a,b)=> b.name.localeCompare(a.name));
+    // fruits_dict.sort((a,b)=> a.calories - b.calories); // Numeric
+    // fruits_dict.sort((a,b)=> b.calories - a.calories); // Reverse numeric
 
-    const fruitsItem = fruits.map(item=><li>{item}</li>);
-    const fuitesItem_dict = fruits_dict.map(item=><li key = {item.id}> 
+    // filter the fruit which is under 50 calories
+    // const lowCal_fruits = fruits_dict.filter(item => item.calories <= 50);
+
+    // const fruitsItem = fruits.map(item=><li>{item}</li>);
+    
+    const fuitesItem_dict = insertList.map(item=><li key = {item.id}> 
             {item.name}: {item.color} &nbsp; Calories: <b>{item.calories}</b></li>);
-    return (<ol>{fuitesItem_dict}</ol>);
+    return (
+        <div>
+            <h2 className = "list-category">{props.category}</h2>
+            <ol className = "list-items">{fuitesItem_dict}</ol>
+        </div>
+    );
 }
+List.proptypes = {
+    items: PropTypes.arrayOf(PropTypes.shape({
+        id: PropTypes.number.isRequired,
+        name: PropTypes.string.isRequired,
+        color: PropTypes.string.isRequired,
+        calories: PropTypes.number.isRequired,
+    })),
+    category: PropTypes.string,
+}
+List.defaultProps = {
+    items: [],
+    category: "Unknown",
+}  
 
 export default List;
