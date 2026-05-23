8864d5478ec017f543aae174e5e20b33448c1334
mingyun <jacky.hsu.tw1@gmail.com>
2026-05-23 10:51:37 +0800

    complete to know how to generate Header and footer in web also how to import the Header and Footer in app

---
+++ my-react-project/src/Footer.jsx
@@ -0,0 +1,10 @@
+function Footer() {
+    return (
+        <footer>
+        <p>&copy; {new Date().getFullYear()} Sponsor: Ming yun, Phone: xxx-xxxx</p>
+        </footer>
+    );
+
+}
+
+export default Footer
