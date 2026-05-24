94fa461d9021c25af7e7e55e67e6c4901026df45
mingyun <jacky.hsu.tw1@gmail.com>
2026-05-24 10:02:15 +0800

    practice radio button and drilldown menu

--- my-react-project/src/OnChangeHooks.jsx
+++ my-react-project/src/OnChangeHooks.jsx
@@ -3,6 +3,8 @@ function OnChangeHooks() {
     const [name, setName] = useState("fff");
     const [quantity, setQuantity] = useState(0);
     const [comment, sentComment] = useState("");
+    const [payment, setPayment] = useState("");
+    const [shipping, setShoping] = useState("Delivery");
 
     
     function handleNameChange(event){
@@ -17,6 +19,14 @@ function OnChangeHooks() {
         sentComment(event.target.value);
     }
 
+    function handlePaymentChange(event){
+        setPayment(event.target.value);
+    }
+
+    function handleShippingChange(event){   
+        setShoping(event.target.value);
+    }
+
     // const handleCommentChanges2 = (event) => {
     //     sentComment(event.target.value);
     // }
@@ -28,7 +38,28 @@ function OnChangeHooks() {
             <input value = {quantity} onChange={handleQuantityChange} type = "number"/>
             <p>Quantity: {quantity}</p>
             <textarea value = {comment} onChange={handleCommentChange} placeholder="Enter delivery instructions"/>
-            <p>Comment: {comment}</p>        
+            <p>Comment: {comment}</p>     
+            
+            <select value = {payment} onChange={handlePaymentChange}>
+                <option value = "">Select and option</option>
+                <option value = "Visa">Visa</option>
+                <option value = "PayPal">PayPal</option>
+                <option value = "Bitcoin">Bitcoin</option>    
+            </select>   
+            <p>Payment: {payment}</p>
+
+            <div>
+                <label>
+                    <input type="radio" value="Pick up" checked={shipping === "Pick up"} onChange={handleShippingChange}/>
+                    Pick up
+                </label>
+                <label>
+                    <input type="radio" value="Delivery" checked={shipping === "Delivery"} onChange={handleShippingChange}/>
+                    Delivery
+                </label>
+                <p>Shipping: {shipping}</p>
+            </div>
+
         </div>
     )
 
